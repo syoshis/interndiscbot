a3b5f0030a8ab35d705d317d382a848c99b7955b
Yoshikatsu Sumitomo <yoshis52102@gmail.com>
2023-05-18 11:50:34 -0400

    basic discord bot with slash commands

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const{Client, GatwatIntentBits, GatewayIntentBits} = require('discord.js')
-require('dotenv/config')
+require('dotenv').config()
 
 const client = new Client({
     intents:[
@@ -13,9 +13,15 @@ client.on('ready',() => {
     console.log("Bot is online!")
 })
 
-client.on('messageCreate', message => {
-    if(message.content === 'ping'){
-        message.reply('pong!')
+client.on('interactionCreate', (interaction) => {
+    if (!interaction.isChatInputCommand()) return;
+  
+    if (interaction.commandName === 'hey') {
+      return interaction.reply('hey!');
     }
-})
+  
+    if (interaction.commandName === 'ping') {
+      return interaction.reply('Pong!');
+    }
+  });
 client.login(process.env.TOKEN)
